a7121d84cfc5ed6243f96767a968db8e0f66f683
it21114526 Mawjood <87365366+sharafMawjood@users.noreply.github.com>
2021-07-13 10:04:00 +0000

    Tute 01

--- Tute01.c
+++ Tute01.c
@@ -5,7 +5,18 @@
 #include <stdio.h>
 
 int main() {
-  
+  float mark1, mark2, avg;
+
+  printf("Enter the first mark: ");
+  scanf("%f", &mark1);
+
+  printf("Enter the second mark: ");
+  scanf("%f", &mark2);
+
+  avg = (mark1 + mark2)/2;
+
+  printf("Avarage of two marks: %.2f", avg);
+
   return 0;
 }
 
